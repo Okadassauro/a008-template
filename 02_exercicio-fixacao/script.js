f4c55feee2f88aa662fa1b12c29a816569bbbf52
Gabriel Oliveira <gabrieloliveira.okada@gmail.com>
2022-07-20 21:18:48 -0300

    exercicios prontos

--- 02_exercicio-fixacao/script.js
+++ 02_exercicio-fixacao/script.js
@@ -2,8 +2,23 @@
 const minhaIdade = prompt('Qual sua idade?')
 const idadeAmiga = prompt('Qual a idade da sua amiga ou amigo?')
 
+
+const MinhaIdade = 26
+
+const MelhorAmigo = 28
+
+console.log('Minha idade é maior do que a do meu melhor amigo?', MinhaIdade > MelhorAmigo )
+
+
+
 // EXERCICIO 02
 const a = 'vou pra praia'
 const b = 'sou maior de idade'
-const c = 'posso tomar uma cervejinha'
+const c = 'posso tomar uma cervejinha' 
 const d = 'quero passear de carro'
+
+
+// !b && d
+// (b && a) && !d
+// a || b
+// c && !d
